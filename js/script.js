fccf72274e0ec82101e8ebb56946e107d5bb1fdf
Gtp87 <fabio.giannitrapani@gmail.com>
2021-11-23 15:12:56 +0100

    creati div, inseriti i dentro div

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
-// Scrivi un programma che stampi in console i numeri da 1 a 100
+// stampo una griglia
+const container = document.querySelector('.container');
 
+// Scrivi un programma che stampi in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+    container.append(div);
+    div.append(i);
 
     // se dividsibile per 3 e 5 scrivi FizzBuzz
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz');
+        div.innerHTML = 'FizzBuzz';
     // se divisibile per 3 scrivi Fizz
     } else if (i % 3 == 0) {
-        console.log('Fizz');
+        div.innerHTML = 'Fizz';
     // se divisibile per 5 scrivi Buzz
     } else if (i % 5 == 0) {
-        console.log('Buzz');
+        div.innerHTML = 'Buzz';
     } else {
         console.log(i);
     }
